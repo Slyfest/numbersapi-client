3d8fc917af2c3bc7562179f6a0c5c55a6e9f9e73
Slyfest <mellowstripe@gmail.com>
2021-03-14 13:25:21 +0600

    Add test cases for trivia type and number response

--- tests/test_numbersapi_client.py
+++ tests/test_numbersapi_client.py
@@ -1,5 +1,20 @@
+import pytest
 from numbersapi_client import __version__
+from numbersapi_client.client import NumbersAPIClient
+from numbersapi_client.response_types import NumberResponse
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.fixture()
+def client():
+    return NumbersAPIClient()
+
+
+def test_trivia(client):
+    number = 42
+    response = client.trivia(number)
+    assert response.number == number
+
+
+def test_number_response(client):
+    response = client.trivia()
+    assert isinstance(response, NumberResponse)
